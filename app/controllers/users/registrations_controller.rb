418c6ea030eb8bfe737db4ddb5c6ad2fe0ac12a3
Yuri1031 <142709785+Yuri1031@users.noreply.github.com>
2025-03-29 13:29:25 +0900

    login&register_view

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  def after_new_user_session_path_for(resource)
+    categories_path
+  end
+
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
   
   def after_update_path_for(resource)
-    mypage_path  # 更新後に /mypage にリダイレクトする
+    mypage_path
   end
 
 end

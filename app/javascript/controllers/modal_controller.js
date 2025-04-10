275926d757a1b8476675028e9caa1a5f6deea4a4
Yuri1031 <142709785+Yuri1031@users.noreply.github.com>
2025-04-10 21:24:07 +0900

    025.04.10②

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,12 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["modal"]
-
   
 
   open() {
     this.modalTarget.style.display = "block"
-    document.getElementById('search_result').innerHTML = '';
   }
 
   close() {
@@ -16,7 +14,7 @@ export default class extends Controller {
     const form = document.getElementById('friend_search_form');
     if (form) {
       form.reset();
-      document.getElementById('search_result').innerHTML = ''; // 検索結果もリセット
+      document.getElementById('search_result').innerHTML = ''; 
     }
   }
 }

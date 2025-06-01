922979a89468cd3b9d06da2b1cd36ad83419653b
Yuri1031 <142709785+Yuri1031@users.noreply.github.com>
2025-06-01 11:16:49 +0900

    修正3

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
 app_path = File.expand_path('../../', __FILE__)
 
-#アプリケーションサーバの性能を決定する
+#アプリケーションサーバの性能を決定
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
@@ -20,9 +20,8 @@ stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
-timeout 60
+timeout 120
 
-#以下は応用的な設定なので説明は割愛
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true

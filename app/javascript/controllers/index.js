daf592c24569bd69d562bb563fc6fe8a1a968d1d
Yuri1031 <142709785+Yuri1031@users.noreply.github.com>
2025-06-07 12:02:08 +0900

    修正12

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,34 +2,34 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "controllers/application.js"
+import { application } from "controllers/application"
 
-import ClickAnswerController from "controllers/click_answer_controller.js"
+import ClickAnswerController from "controllers/click_answer_controller"
 application.register("click-answer", ClickAnswerController)
 
-import ColorController from "controllers/color_controller.js"
+import ColorController from "controllers/color_controller"
 application.register("color", ColorController)
 
-import HelloController from "controllers/hello_controller.js"
+import HelloController from "controllers/hello_controller"
 application.register("hello", HelloController)
 
-import HoverColorController from "controllers/hover_color_controller.js"
+import HoverColorController from "controllers/hover_color_controller"
 application.register("hover-color", HoverColorController)
 
-import MenuController from "controllers/menu_controller.js"
+import MenuController from "controllers/menu_controller"
 application.register("menu", MenuController)
 
-import ModalController from "controllers/modal_controller.js"
+import ModalController from "controllers/modal_controller"
 application.register("modal", ModalController)
 
-import SearchController from "controllers/search_controller.js"
+import SearchController from "controllers/search_controller"
 application.register("search", SearchController)
 
-import ShareController from "controllers/share_controller.js"
+import ShareController from "controllers/share_controller"
 application.register("share", ShareController)
 
-import ToggleController from "controllers/toggle_controller.js"
+import ToggleController from "controllers/toggle_controller"
 application.register("toggle", ToggleController)
 
-import WordMarkController from "controllers/word_mark_controller.js"
+import WordMarkController from "controllers/word_mark_controller"
 application.register("word-mark", WordMarkController)
